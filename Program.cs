6a264883eab65acb33fee98641039a61d14e1c5e
Anmar PC <aj@anmarjarjees.com>
2023-07-27 20:04:13 -0400

    Modifications

--- Program.cs
+++ Program.cs
@@ -29,17 +29,33 @@ namespace MovieMVC
             /*
             NOTE:
             This code for "MapControllerRoute" it used to be written in Startup.cs file,
-            but in .NET 6 and later => no more Startup.cs file            
+            but in .NET 6 and later => no more Startup.cs file   
+            Link: https://learn.microsoft.com/en-us/aspnet/core/tutorials/first-mvc-app/adding-controller?view=aspnetcore-7.0&tabs=visual-studio#add-a-controller
             */
             app.MapControllerRoute(
-                // The default router control
+                // The default router control: /[Controller]/[ActionName]/[Parameters] 
                 name: "default",
                 /*
+                The routing format is set by default:
                 > controller=Home
-                Inside "Views" => "Home" folder 
-                > action=Index => "Index.cshtml" is the default home page within the "Home" folder
-                 */
+                    Inside "Views" => "Home" folder 
+                > action=Index => "Index.cshtml" 
+                    "Index" is the default home page within the "Home" folder
+                */
                 pattern: "{controller=Home}/{action=Index}/{id?}");
+            /*
+            When you launch the application without specifying any URL segments, 
+            it defaults to the "Home" controller and the "Index" method specified in the template line
+
+            To run the application with our Controller, we need to modify the URL:localhost:xxxx/HelloWorld
+
+            Browse to: 
+            - https://localhost:{PORT}/HelloWorld
+                > will run the default action method "Index"
+            - https://localhost:{PORT}/HelloWorld/Welcome. 
+                > will run the action method "Welcome"
+            Replace {PORT} with your port number.
+            */
 
             app.Run();
         }
